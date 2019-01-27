9285ace5727beef859f3b9d57e2676f6f1d3090c
Hannah Carnes <32318292+hcarnes@users.noreply.github.com>
2019-01-27 15:49:00 -0500

    reorg components (#2)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
-const EstablishmentSelector = () => {
-  return (
-    <>
-      <Link to="/12345">Hanny Cafe</Link>
-    </>
-  );
-};
-const EstablishmentDetail = ({ match }) => <h2>CAMID: {match.params.camid}</h2>;
-
+import EstablishmentDetail from './components/EstablishmentDetail'
+import EstablishmentSelector from './components/EstablishmentSelector'
 class App extends Component {
   render() {
     return (
---
+++ src/components/EstablishmentSelector.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const EstablishmentSelector = () => {
+  return (
+    <>
+      <Link to="/12345">Hanny Cafe</Link>
+    </>
+  );
+};
+
+export default EstablishmentSelector;
