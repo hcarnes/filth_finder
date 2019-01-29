615bdd96670119eeb034a81d9f077f32095adf55
Hannah Carnes <hecarnes@gmail.com>
2019-01-29 14:35:58 -0500

    format EstablishmentSelector

--- src/components/EstablishmentSelector.js
+++ src/components/EstablishmentSelector.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Establishment from '../models/Establishment'
 import EstablishmentList from "./EstablishmentList";
+import { Heading, Box } from 'grommet';
 
 const EstablishmentSelector = () => {
   return (
     <>
-      <h1>Establishments near you:</h1>
+    <Box align="center">
+      <Heading>Establishments near you:</Heading>
       <EstablishmentList establishments={Establishment.near(1,1)}/>
+     </Box>
     </>
   );
 };
