28440f0b9b136373b83ee2e1f141723484b8be76
Hannah Carnes <hecarnes@gmail.com>
2019-01-29 14:36:12 -0500

    format EstablishmentList

--- src/components/EstablishmentList.js
+++ src/components/EstablishmentList.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from './EstablishmentList.module.css'
 
 const EstablishmentList = ({ establishments }) => {
   return (
-    <>
-      <ul>
+    <>      
+      <ul className={styles.EstablishmentList}>
         {establishments.map(establishment => {
           return (
-            <li>
+            <li key={establishment.dba}>
               <Link to={`/${establishment.camis}`}>{establishment.dba} - {establishment.grade}</Link>
             </li>
           );
