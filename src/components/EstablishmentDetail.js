9b4c1a35d21782576af6d20a6c03de86af6a3348
Hannah Carnes <hecarnes@gmail.com>
2019-01-31 17:03:39 -0500

    drop down on accordian animates on open tab

--- src/components/EstablishmentDetail.js
+++ src/components/EstablishmentDetail.js
@@ -1,69 +1,72 @@
 import React from "react";
 import Establishment from "../models/Establishment";
 import styles from "./EstablishmentDetail.module.css";
-import { Heading, Text, Accordion, AccordionPanel, Box } from "grommet";
-
-const renderPanelHeader = title => (
-  <Box direction="row" align="center" pad="medium" gap="small">
-    <strong>
-      <Text>{title}</Text>
-    </strong>
-  </Box>
-);
+import {
+  Heading,
+  Text,
+  Accordion,
+  AccordionPanel,
+  Box,
+} from "grommet";
 class EstablishmentDetail extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { camis: props.match.params.camis, establishment: null };
+    this.state = {
+      camis: props.match.params.camis,
+      establishment: null
+    };
   }
 
   componentDidMount = async () => {
     const establishment = await Establishment.detail(this.state.camis);
     this.setState({ establishment });
-    console.log(establishment);
   };
 
   render = () => {
     const establishment = this.state.establishment;
+
     if (establishment) {
       return (
         <>
-          
+          <Box align="center">
             <Heading>Violations at {establishment.dba}</Heading>
-            
+
             <Text>CAMIS: {this.state.camis}</Text>
-            <ul className={styles.EstablishmentDetail}>
-              <Accordion>
-                {establishment.inspections.map(inspection => {
-                  return (
-                    <AccordionPanel header={renderPanelHeader(`${inspection.date} - ${inspection.grade}`)}>
-                      <li key={inspection.date}>
-                        <Box
-                          pad="medium"
-                          background="light-2"
-                          style={{ textAlign: "left" }}
-                        >
-                          <Text>Grade: {inspection.grade}</Text>
-                          <br />
-                          <Text>Violations:</Text>
-                          <ul className={styles.EstablishmentDetail}>
-                            {inspection.violations.map(v => {
-                              return (
-                                <li key={inspection.date + v.code}>
-                                  <p>
-                                    {v.code} - {v.description}
-                                  </p>
-                                </li>
-                              );
-                            })}
-                          </ul>
-                        </Box>
-                      </li>
-                    </AccordionPanel>
-                  );
-                })}
-              </Accordion>
-            </ul>
-          
+          </Box>
+          <ul className={styles.EstablishmentDetail}>
+            <Accordion animate="animate" multipe="multiple">
+              {establishment.inspections.map((inspection) => {
+                return (
+                  <AccordionPanel
+                    label={`${inspection.date} - ${inspection.grade}`}>
+                    <Box background="light-2" style={{height: "50px"}}></Box>
+                    <li key={inspection.date}>
+                      <Box
+                        pad="medium"
+                        background="light-2"
+                        style={{ textAlign: "left" }}
+                      >
+                        <Text>Grade: {inspection.grade}</Text>
+                        <br />
+                        <Text>Violations:</Text>
+                        <ul className={styles.EstablishmentDetail}>
+                          {inspection.violations.map(v => {
+                            return (
+                              <li key={inspection.date + v.code}>
+                                <p>
+                                  {v.code} - {v.description}
+                                </p>
+                              </li>
+                            );
+                          })}
+                        </ul>
+                      </Box>
+                    </li>
+                  </AccordionPanel>
+                );
+              })}
+            </Accordion>
+          </ul>
         </>
       );
     } else {
