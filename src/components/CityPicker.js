82c6507f27c80068d33f0eaff71d910ef11fbdb5
Hannah Carnes <hecarnes@gmail.com>
2019-06-11 13:47:56 -0400

    style city picker

--- src/components/CityPicker.js
+++ src/components/CityPicker.js
@@ -1,18 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Heading, Box } from "grommet";
+import styles from "./CityPicker.module.css";
 
 const CityPicker = () => {
   return (
-    <>
-      <ul>
+    <Box align="center">
+      <Heading color="brand">Select a city:</Heading>
+      <ul className={styles.CityPicker}>
         <li>
+          <span role="img" aria-label="coffee">
+            ☕
+          </span>
           <Link to="/seattle">Seattle</Link>
         </li>
         <li>
+          <span role="img" aria-label="statue of liberty">
+            🗽
+          </span>
           <Link to="/nyc">NYC</Link>
         </li>
       </ul>
-    </>
+    </Box>
   );
 };
 
